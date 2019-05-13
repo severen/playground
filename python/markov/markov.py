63ecf410764d468575a696af008d817a41fdb1e5
Severen Redwood <severen@shrike.me>
2019-05-14 01:30:20 +1200

    Refactor markov.py to be reusable

--- python/markov/markov.py
+++ python/markov/markov.py
@@ -10,18 +10,6 @@ import argparse
 from pathlib import Path
 from collections import defaultdict, Counter
 
-parser = argparse.ArgumentParser(
-  description='Generate text from a corpus with a Markov chain.'
-)
-parser.add_argument('corpus', type=str, help='The file path to the input text.')
-parser.add_argument(
-  '-o', '--order',
-  type=int, default=1,
-  help='The order (amount of words to consider) of the Markov chain.'
-)
-
-args = parser.parse_args()
-
 # Characters that text generation should end on.
 # TODO: Handle quotation marks and brackets.
 STOP_CHARACTERS = (
@@ -34,33 +22,57 @@ STOP_CHARACTERS = (
   '...',
 )
 
-corpus = Path(args.corpus).read_text().rstrip().split()
-model = defaultdict(Counter)
+class Chain:
+  def __init__(self, order):
+    self.model = defaultdict(Counter)
+    self.order = order
 
-print('Training model...')
-# TODO: Write/use a more idiomatic iterator for this.
-for i in range(len(corpus)):
-  # Do not record the last state/word in the corpus since it has no following
-  # words.
-  if i == len(corpus) - (args.order + 1):
-    break
+  def train(self, corpus):
+    # TODO: Write/use a more idiomatic iterator for this.
+    for i in range(len(corpus)):
+      # Do not record the last state/word in the corpus since it has no following
+      # words.
+      if i == len(corpus) - (self.order + 1):
+        break
 
-  key = ' '.join(corpus[i:i + args.order])
-  val = ' '.join(corpus[i + args.order:i + 2 * args.order])
+      key = ' '.join(corpus[i:i + self.order])
+      val = ' '.join(corpus[i + self.order:i + 2 * self.order])
 
-  model[key].update([val])
+      self.model[key].update([val])
 
-print('Sampling...')
-state = random.choice(list(model))
-out = [state]
+  def generate(self):
+    state = random.choice(list(self.model))
+    out = [state]
 
-for i in range(50):
-  choice = random.choices(list(model[state]), model[state].values())
-  state = choice[0]
+    for i in range(50):
+      choice = random.choices(list(self.model[state]), self.model[state].values())
+      state = choice[0]
 
-  out.extend(choice)
+      out.extend(choice)
 
-  if choice[0].endswith(STOP_CHARACTERS):
-    break
+      if choice[0].endswith(STOP_CHARACTERS):
+        break
 
-print(' '.join(out))
+    return ' '.join(out)
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser(
+    description='Generate text from a corpus with a Markov chain.'
+  )
+  parser.add_argument('corpus', type=str, help='The file path to the input text.')
+  parser.add_argument(
+    '-o', '--order',
+    type=int, default=1,
+    help='The order (amount of words to consider) of the Markov chain.'
+  )
+
+  args = parser.parse_args()
+
+  corpus = Path(args.corpus).read_text().rstrip().split()
+  chain = Chain(args.order)
+
+  print('Training model...')
+  chain.train(corpus)
+
+  print('Generating text from model...')
+  print(chain.generate())
