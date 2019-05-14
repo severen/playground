478cb0bda7538ae3107f4bde50287eb4419349c7
Severen Redwood <severen@shrike.me>
2019-05-14 14:00:22 +1200

    Reformat and document markov.py

--- python/markov/markov.py
+++ python/markov/markov.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
-# A simple Markov chain-powered text generator.
-#
-# You can run this code with `python -X utf8 markov.py`.
+"""
+A simple Markov chain-powered text generator.
+
+You can run this code with `python -X utf8 markov.py`, or import it in another
+file and use the Chain class directly.
+"""
 
 import random
 import argparse
@@ -13,66 +16,78 @@ from collections import defaultdict, Counter
 # Characters that text generation should end on.
 # TODO: Handle quotation marks and brackets.
 STOP_CHARACTERS = (
-  '.',
-  '?',
-  '!',
-  '…',
-  # This is *technically* not a character, but since no one actually uses
-  # Unicode's ellipses character we have to include this.
-  '...',
+    ".",
+    "?",
+    "!",
+    "…",
+    # This is *technically* not a character, but since no one actually uses
+    # Unicode's ellipses character we have to include this.
+    "...",
 )
 
+
 class Chain:
-  def __init__(self, order):
-    self.model = defaultdict(Counter)
-    self.order = order
+    """An nth-order Markov chain specialised for text generation given an input corpus."""
 
-  def train(self, corpus):
-    # TODO: Write/use a more idiomatic iterator for this.
-    for i in range(len(corpus)):
-      # Do not record the last state/word in the corpus since it has no following
-      # words.
-      if i == len(corpus) - (self.order + 1):
-        break
+    def __init__(self, order):
+        self.model = defaultdict(Counter)
+        self.order = order
 
-      key = ' '.join(corpus[i:i + self.order])
-      val = ' '.join(corpus[i + self.order:i + 2 * self.order])
+    def train(self, corpus):
+        """Train the chain's model with the given input corpus."""
+        # TODO: Write/use a more idiomatic iterator for this.
+        for i in range(len(corpus)):
+            # Do not record the last state/word in the corpus since it has no following
+            # words.
+            if i == len(corpus) - (self.order + 1):
+                break
 
-      self.model[key].update([val])
+            key = " ".join(corpus[i : i + self.order])
+            val = " ".join(corpus[i + self.order : i + 2 * self.order])
 
-  def generate(self):
-    state = random.choice(list(self.model))
-    out = [state]
+            self.model[key].update([val])
 
-    for i in range(50):
-      choice = random.choices(list(self.model[state]), self.model[state].values())
-      state = choice[0]
+    def generate(self):
+        """Generate a random string based upon the chain's current model."""
+        state = random.choice(list(self.model))
+        out = [state]
 
-      out.extend(choice)
+        for _ in range(50):
+            choice = random.choices(list(self.model[state]), self.model[state].values())
+            state = choice[0]
 
-      if choice[0].endswith(STOP_CHARACTERS):
-        break
+            out.extend(choice)
 
-    return ' '.join(out)
+            if choice[0].endswith(STOP_CHARACTERS):
+                break
 
-if __name__ == '__main__':
-  parser = argparse.ArgumentParser(
-    description='Generate text from a corpus with a Markov chain.'
-  )
-  parser.add_argument('corpus', type=str, help='The file path to the input text.')
-  parser.add_argument(
-    '-o', '--order',
-    type=int, default=1,
-    help='The order (amount of words to consider) of the Markov chain.'
-  )
+        return " ".join(out)
 
-  args = parser.parse_args()
 
-  corpus = Path(args.corpus).read_text().rstrip().split()
-  chain = Chain(args.order)
+def main():
+    """Main executable entry point."""
+    parser = argparse.ArgumentParser(
+        description="Generate text from a corpus with a Markov chain."
+    )
+    parser.add_argument("corpus", type=str, help="The file path to the input text.")
+    parser.add_argument(
+        "-o",
+        "--order",
+        type=int,
+        default=1,
+        help="The order (amount of words to consider) of the Markov chain.",
+    )
 
-  print('Training model...')
-  chain.train(corpus)
+    args = parser.parse_args()
 
-  print('Generating text from model...')
-  print(chain.generate())
+    chain = Chain(args.order)
+
+    print("Training model...")
+    chain.train(Path(args.corpus).read_text().rstrip().split())
+
+    print("Generating text from model...")
+    print(chain.generate())
+
+
+if __name__ == "__main__":
+    main()
